a1c613b63a1bba2b3a52403499ae885b31e6121b
Aviral <avimittal30@gmail.com>
2024-04-30 20:06:47 +0530

    store index added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Neuropathic Chatbot',
+    version= '0.0.0',
+    author= 'Aviral Mittal',
+    author_email= 'avimittal30@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
